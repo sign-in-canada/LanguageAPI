3d46962b9749ee0ed238ac4a6b2f43ec019745af
Doug Harris <doug.harris@xsilosystems.com>
2019-06-27 13:28:38 -0400

    Create a simple example for testing

---
+++ client/js/languageAPI.js
@@ -0,0 +1,26 @@
+function getLanguage(callback) {
+    $.ajax({
+        url: 'https://server.fjgc-gccf.gc.ca/api/Prefs',
+        type: 'GET',
+        contentType: 'application/json',
+        xhrFields: {
+            withCredentials: true
+        },
+        success: function (data) { console.log(data); callback(data.lang) },
+        error: function (jqXHR, textStatus, errorThrown) { console.log(errorThrown); callback(textStatus)}
+    });
+}
+
+function setLanguage(language) {
+    $.ajax({
+        url: 'https://server.fjgc-gccf.gc.ca/api/Prefs',
+        type: 'PUT',
+        datatype: 'application/json',
+        contentType: 'application/json',
+        async: false,
+        data: `{lang: \"${language}\"}`,
+        xhrFields: {
+            withCredentials: true
+        }
+    });
+}
