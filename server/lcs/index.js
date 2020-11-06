1f18e170cf63a10ae33243751350e4a5a8f12c10
Doug M. Harris <dharris@hq.xsilo.com>
2020-11-06 10:56:54 -0500

    Add an old-style redirect service
    THis works with the old SAML RP Simulators

---
+++ server/lcs/index.js
@@ -0,0 +1,39 @@
+module.exports = async function (context, req) {
+    const host = req.headers.host;
+    const commonDomain = host.substring(host.indexOf('.'));
+
+    let target = req.query.return || req.headers.referer;
+    let lang = req.query.lang;
+    let idp;
+    let clf = req.query.clf;
+
+    context.res = {
+        status: 302,
+        body: null
+    }
+
+    if (lang && ["eng", "fra"].includes(lang)) {  // Set the cookie
+        context.res.headers = {
+            "Set-Cookie" : "_gc_lang=" + lang + "; domain=" + commonDomain + "; path=/; SameSite=None; secure; HttpOnly",
+            "Location" : target
+            }
+    }
+    else {  //Read the cookie
+        context.log("Reading cookie")
+        if (typeof req.headers != "undefined" && "cookie" in req.headers) {
+            if ((matches = req.headers.cookie.match("_gc_lang=(eng|fra)")) != null) {
+                lang = matches[1];
+            }
+            if ((matches = req.headers.cookie.match("_saml_idp=([^;/s]*)")) != null) {
+                idp = matches[1];
+            }
+
+        target = target.replace(/-(eng|fra).jsp/, "-" + lang + ".jsp")
+                + "?_gc_lang=" + lang + "&_saml_idp=" + idp
+
+        context.res.headers = {
+            "Location" : target
+            }
+        }
+    }
+}
